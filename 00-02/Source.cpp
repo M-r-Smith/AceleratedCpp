8caa27e17c3bd836d4c37d25b7a2a1ce4491f55c
rozu-urameshi <73414361+rozu-urameshi@users.noreply.github.com>
2022-01-17 12:06:58 -0500

    Exercise

---
+++ 00-02/Source.cpp
@@ -0,0 +1,14 @@
+/*
+0-2
+Write a program that, when run, writes
+	This (*) is as quote, and this (\) is a backslash.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "This (*) is as quote, and this (\\) is a backslash." << endl;
+	return 0;
+}
