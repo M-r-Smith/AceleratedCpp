215509104297a573f2c5e661f04a83ed15b3adc7
rozu-urameshi <73414361+rozu-urameshi@users.noreply.github.com>
2022-01-18 09:30:58 -0500

    Exercise

---
+++ 00-06/Source.cpp
@@ -0,0 +1,17 @@
+/*
+0-6
+Is this a valid program? Why or Why not?
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	{ { { { { { cout << "Hello, world!" << endl; }}}}}}
+}
+
+/*
+Yes this is a program because the character output statement is inside the curly braces 
+of the main function.
+*/
