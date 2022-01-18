0babd07bc7bf3cdf524b684f5d3cf99c2dab4c65
rozu-urameshi <73414361+rozu-urameshi@users.noreply.github.com>
2022-01-18 09:25:20 -0500

    Exercise

---
+++ 00-05/Source.cpp
@@ -0,0 +1,14 @@
+/*
+Is this a valid program? Why or why not?
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+cout << "Hello, world!" << endl;
+
+/*
+No because it is missing the curly braces that indicate that the character output is apart
+of the main function.
+*/
