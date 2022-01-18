7a781ea3738ee91a7ff5ea7f53bd2a2a31dfcb1e
rozu-urameshi <73414361+rozu-urameshi@users.noreply.github.com>
2022-01-18 09:38:22 -0500

    Exercises

---
+++ 00-07/Source.cpp
@@ -0,0 +1,21 @@
+/*
+0-7
+What about this one?
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	  /*
+	  This is a comment that extends over several lines because it uses (/* and */) as its startingand ending delimiters* /
+
+	  cout << "Does this work?" << endl;
+	  return 0;
+}
+
+/*
+The only reason that this does not work is because the example given ends the comment too soon.
+However, if this was anyother comment this would work because you can place a comment anywhere
+*/
