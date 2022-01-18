873a340a4c00076d797e931ad22e496937f01c4f
rozu-urameshi <73414361+rozu-urameshi@users.noreply.github.com>
2022-01-18 08:51:09 -0500

    Exercise

---
+++ 00-04/Source.cpp
@@ -0,0 +1,12 @@
+/*
+0-4
+Write a program that, when run, writes the Hello, world! program as its output.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello, world!" << endl;
+}
