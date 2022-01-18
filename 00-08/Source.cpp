b9541f567cd184a35fa175e6b7ef58ef5dab1b15
rozu-urameshi <73414361+rozu-urameshi@users.noreply.github.com>
2022-01-18 10:14:46 -0500

    Exercises

---
+++ 00-08/Source.cpp
@@ -0,0 +1,19 @@
+/*
+0-8
+...and this one?
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//This is a comment that extends over several lines
+	//by using // at the beginning of each line instead of using /*
+	// or */ to delimit comments.
+	cout << "Does this work?" << endl;
+	return 0;
+}
+
+/*
+This comment works and is used just as intended.
+*/
