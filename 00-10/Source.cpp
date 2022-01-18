11de3a7cc44862d1219e98341a132fd7c499ff34
rozu-urameshi <73414361+rozu-urameshi@users.noreply.github.com>
2022-01-18 10:40:43 -0500

    Exercise

---
+++ 00-10/Source.cpp
@@ -0,0 +1,12 @@
+/*
+Rewrite the Hello, world! Program so that a newline occurs everywhere that whitespace is allowed in the program.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello,\nworld! \n" << endl;
+	return 0;
+}
