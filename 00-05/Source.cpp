67b51def1d02a1fe702b10769f4facca0550d804
rozu-urameshi <73414361+rozu-urameshi@users.noreply.github.com>
2022-01-18 09:01:05 -0500

    Exercise

---
+++ 00-05/Source.cpp
@@ -0,0 +1,15 @@
+/*
+Is this a valid program? Why or why not?
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{ { { { {cout << "Hello, world!" << endl; }}}}
+
+}
+
+/*
+Yes this is a valid program because the character output is still imbeded inside of the main function.
+*/
